aebf78d05a5d7d1c75f62af1108d1bff744265f3
s434765 <karolinaoparczyk@gmail.com>
2019-04-03 16:53:52 +0200

    dfs tips

--- helpers.py
+++ helpers.py
@@ -74,38 +74,22 @@ def color_grid(grid):
     return all_sprites_list, garbage_collector, houses
 
 
-def auto_map(WINDOW_SIZE):
-    all_sprites_list = pygame.sprite.Group()
-    white_boxes = []
-    grasses = []
-    roads = []
-    houses = []
-    for i in range(0, 930, 30):
-        for j in range(0, 930, 30):
-            if i == 0 and j == 0:
-                pass
-            elif j == 0:
-                white_boxes.append(Area(None, i, j))
-            elif i % 8 == 0 or j % 8 == 0:
-                roads.append(Area('road', i, j + 30))
-            else:
-                a = random.randrange(0, 900, 30)
-                b = random.randrange(0, 900, 30)
-                c = random.randrange(0, 900, 30)
-                d = random.randrange(0, 900, 30)
-                e = random.randrange(0, 900, 30)
-                if j % 60 != 0 and (
-                        j == a or j == b or j == c or j == d or j == e):  # nie budujemy domu w środku kwadratu
-                    houses.append(House(100, i, j + 30))
-                else:
-                    grasses.append(Area('grass', i, j + 30))
+# DFS
+# rekurencją
+# węzłem drzewa konkrenta sytuacja na planszy
+# gałęziamy (możliwościami) jest to wszystko, co może zrobic agent
+# zasada: nie powtarzamy ostatniej operacji
+# jak może coś odwiedzić (zebrać śmieci), to niech to zrobi
+# ucinanie ścieżki, jeśli robi zbyt dużo operacji (zapętlenie)
 
-    all_sprites_list.add(white_boxes)
-    all_sprites_list.add(grasses)
-    all_sprites_list.add(roads)
-    all_sprites_list.add(houses)
-    garbage_dump = Area('garbage_dump', 0, 0)
-    all_sprites_list.add(garbage_dump)
-    garbage_collector = GarbageCollector(250, WINDOW_SIZE, grasses, houses, garbage_dump, white_boxes, 0, 30)
-    all_sprites_list.add(garbage_collector)
-    return all_sprites_list, garbage_collector
+#przyjmuje ciąd dotyczhczasowych operacji
+# dfs_find(GRID, CURRENT_OPERATIONS)
+# 1. sprawdź czy koniec (czy zadanie zostało wykonane/czy wszystkie punkty zostały wykonane)
+# a) TAK
+#   ..globalna SOLUTIONS = []..
+#   SOLUTIONS.append(current_operations)
+# b) NIE
+#   Sprawdź możliwości
+#   ..operacje na gridie.. funkcja dodatkowa - przyjmuje grida i operację, zwraca zmienionego grida
+#   .. zwiększamy liste operacji..
+#   dfs_find(zmieniony grid, zwiekszona lista operacji)
